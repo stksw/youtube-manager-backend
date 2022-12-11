caebfef4b1d894540da193a25684072fa210213f
stksw <kakiage@gmail.com>
2022-12-11 11:04:53 +0900

    impl GetVideo api

--- routes/api.go
+++ routes/api.go
@@ -10,6 +10,9 @@ func Init(e *echo.Echo) {
 	g := e.Group("/api")
 	{
 		g.GET("/popular", api.FetchMostPopularVideos())
+		g.GET("/video/:id", api.GetVideo())
+		g.GET("/related/:id", api.FetchRelatedVideos())
+		g.GET("/search", api.SearchVideos())
 	}
 
 }
